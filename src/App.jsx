f4e91f2c41b6bf1473479ba91357a0c991005981
Manuel Manna <manuelmannadev@gmail.com>
2023-03-06 17:57:49 +0100

    Immagine ultimata

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import chuck from './assets/chuc_desktop.svg'
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <div id='container'>
       <h1 id='title'>Webapp API Chuck Norris</h1>
       <p id='paragraph'>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</p>
-
+      <img id='chuck_img' src={chuck} alt="Beh, è chuck" />
       </div>
     </div>
   )
