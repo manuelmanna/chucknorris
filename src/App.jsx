042435243784f27a20c2b7008a4c95710c7fea98
Manuel Manna <manuelmannadev@gmail.com>
2023-03-06 17:52:36 +0100

    Terminato il paragrafo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <div id='container'>
       <h1 id='title'>Webapp API Chuck Norris</h1>
-      
+      <p id='paragraph'>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</p>
+
       </div>
     </div>
   )
