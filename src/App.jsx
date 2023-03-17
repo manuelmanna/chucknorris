094c639810e1afb821e068f6ec56a532db37e658
Manuel Manna <manuelmannadev@gmail.com>
2023-03-17 12:38:32 +0100

    risolti conflitti di merge

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import chuck from './assets/chuc_desktop.svg'
 import CategorySelector from './components/CategorySelector'
-import JokeRender from './components/JokeRender'
 import Buttons from './components/Buttons'
+import JokeRender from './components/JokeRender'
+
+
 
 function App() {
-  const [joke, setJoke] = useState("")
   const [categories, setCategories] = useState([])
+  const [joke, setJoke] = useState("")
   const [userselection, setuserselection] = useState("")
+
   function RenderCategories(){
       let url = 'https://api.chucknorris.io/jokes/categories'
 
@@ -18,33 +21,53 @@ function App() {
         response => response.json()
     ).then(
         data => {
-          let obj = data.map(function(item, index){
-            return {
-              id: index,
-              value: item
-            }
-          })
-          setCategories(obj)
-        }  
+          data.forEach(element => {
+            let obj = data.map(function(item, index){
+              return {
+                id: index,
+                value: item
+              }
+            })
+            setCategories(obj)
+          });
+        }
     )
   }
 
   function JokeDisplay(){
-    let url = `https://api.chucknorris.io/jokes/random?category=travel`
-    let promise = fetch(url)
-    
-    promise.then(
-      response => response.json()
-    ).then(
-      data => setJoke(data.value)
-    ) 
+    if(userselection != ""){
+      let url = `https://api.chucknorris.io/jokes/random?category=${userselection}`
+      let promise = fetch(url)
+      
+      promise.then(
+        response => response.json()
+      ).then(
+        data => setJoke(data.value)
+      ) 
+    }else{
+      let url = `https://api.chucknorris.io/jokes/random`
+      let promise = fetch(url)
+      
+      promise.then(
+        response => response.json()
+      ).then(
+        data => setJoke(data.value)
+      ) 
+      
+    }
+  }
+
+  function userSelectionHandler(e){
+    setuserselection(e)
   }
 
   
 
   function copy(){
-    navigator.clipboard.writeText(joke)
-    alert("Il testo è stato copiato")
+    if(joke != ""){
+      navigator.clipboard.writeText(joke)
+      alert("Il testo è stato copiato")
+    }
   }
 
   return (
@@ -52,8 +75,8 @@ function App() {
       <div id='container'>
       <h1 id='title'>Webapp API Chuck Norris</h1>
       <p id='paragraph'>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</p>
-      <img id='chuck_img' src={chuck} alt="Beh, è chuck" />
-      <CategorySelector categories={categories} clbk={RenderCategories} />
+      <img id='chuck_img' src={chuck} alt="Beh, è chuck " />
+      <CategorySelector categories={categories} clbk={RenderCategories} handler={userSelectionHandler} />
       {joke != "" &&
         <JokeRender joke={joke}/>
         } 
--- src/components/CategorySelector.jsx
+++ src/components/CategorySelector.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react'
 import '../styles/CategorySelector.css'
-const CategorySelector = ({categories, clbk}) => {
+const CategorySelector = ({categories, clbk, handler}) => {
 
   useEffect(clbk, [])
 
+  function getCategory(e){
+    handler(e.target.value)
+  }
+
   return (
     <div className='CategorySelector'>
-      <select name="category" id="category">
+      <select name="category" id="category" onChange={(e) => getCategory(e)}>
         <option value="" defaultValue hidden>Categorie</option>
         {categories.map((categories) => 
         <option value={categories.value} key={categories.id}>{categories.value}</option>
